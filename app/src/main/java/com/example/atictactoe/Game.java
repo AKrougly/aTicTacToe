b6a901a5474106f0df53e2798d53298d978d2e53
Антон Круглый <A.Krougly@besmart.by>
2023-05-30 10:25:57 +0300

    you can play with a friend

--- app/src/main/java/com/example/atictactoe/Game.java
+++ app/src/main/java/com/example/atictactoe/Game.java
@@ -4,124 +4,140 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
 
-class Btn {
-    static final char CHAR_SPACE = ' ';
-    static final char CHAR_CROSS = 'X';
-    static final char CHAR_CIRCLE = 'O';
-    private final int idx;
-    private char val;
-    private ImageView imageView;
-    Btn(int idx, char val, ImageView imageView) {
-        this.idx = idx;
-        this.val = val;
-        this.imageView = imageView;
-    }
-    boolean isEmpty() {
-        return val == CHAR_SPACE;
-    }
-    ImageView getComponent() {
-        return imageView;
-    }
-    void bindComponent(ImageView imageView) {
-        this.imageView = imageView;
-    }
-    int getIdx() {
-        return idx;
-    }
-    char getVal() {
-        return val;
-    }
-    void setVal(char val) {
-        this.val = val;
-    }
-}
 public class Game {
-    private final MainActivity mainActivity;
-    Button resetGame;
+    private int winner;
     private int playerNo;
     private char playerImg;
-    private boolean thereIsAWinner;
-    private Btn[] btnArray = new Btn[] {
-            new Btn(0, ' ', null),
-            new Btn(1, ' ', null),
-            new Btn(2, ' ', null),
-            new Btn(3, ' ', null),
-            new Btn(4, ' ', null),
-            new Btn(5, ' ', null),
-            new Btn(6, ' ', null),
-            new Btn(7, ' ', null),
-            new Btn(8, ' ', null)
+    private final TextView playerOneScore;
+    private final TextView playerTwoScore;
+    private final Cell[] cellArray = new Cell[] {
+            new Cell(0, 0, null),
+            new Cell(1, 0, null),
+            new Cell(2, 0, null),
+            new Cell(3, 0, null),
+            new Cell(4, 0, null),
+            new Cell(5, 0, null),
+            new Cell(6, 0, null),
+            new Cell(7, 0, null),
+            new Cell(8, 0, null)
     };
-    private List<Btn> btnList = Arrays.asList(btnArray);
-    private int[][] winningPositions = {
+    private final List<Cell> cellList = Arrays.asList(cellArray);
+    private final int[][] WINNING_COMBOS = {
             {0, 1, 2}, {3, 4, 5}, {6, 7, 8},    //rows
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},    //columns
             {0, 4, 8}, {2, 4, 6}    //cross
     };
     Game(MainActivity mainActivity) {
-        this.mainActivity = mainActivity;
-        playerNo = 0;
-        playerImg = Btn.CHAR_CROSS;
-        thereIsAWinner = false;
-        resetGame = (Button)mainActivity.findViewById(R.id.Reset);
-        resetGame.setOnClickListener(new View.OnClickListener() {
+        playerOneScore = mainActivity.findViewById(R.id.player_1_score);
+        playerTwoScore = mainActivity.findViewById(R.id.player_2_score);
+        setPlayerOneScore("0");
+        setPlayerTwoScore("0");
+        Button resetButton = mainActivity.findViewById(R.id.Reset);
+        resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 resetGame();
             }
         });
 
-        for(Btn btn : btnList) {
+        for(Cell cell : cellList) {
             try {
-                ImageView img = (ImageView) mainActivity.findViewById(mainActivity.getResources().getIdentifier("buttonImage" + btn.getIdx(), "id", mainActivity.getPackageName()));
+                ImageView img = (ImageView) mainActivity.findViewById(
+                    mainActivity.getResources().getIdentifier(
+                        "buttonImage" + cell.getIdx(), "id", mainActivity.getPackageName()
+                    )
+                );
+                cell.bindComponent(img);
                 img.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        btn.bindComponent(img);
-                        makeMove(btn);
+                        makeMove(cell);
                     }
                 });
-            } catch(Exception e) {
-                Log.e("ATicTacToe", "Bad btn:" + btn.getIdx());
+            } catch (Exception e) {
+                Log.e("ATicTacToe", "Bad cell:" + cell.getIdx());
             }
         }
+        resetGame();
     }
-    void makeMove(Btn btn) {
-        if (!thereIsAWinner && btn.isEmpty()) {
-            btn.getComponent().setImageResource(playerImg == Btn.CHAR_CROSS ? R.drawable.cross : R.drawable.circle);
-            btn.setVal(playerImg == Btn.CHAR_CROSS ? Btn.CHAR_CIRCLE : Btn.CHAR_CROSS);
-            thereIsAWinner = checkWinner();
-            if (!thereIsAWinner) {
-                playerNo = playerNo == 0 ? 1 : 0;
-                playerImg = playerImg == Btn.CHAR_CROSS ? Btn.CHAR_CIRCLE : Btn.CHAR_CROSS;
-            }
+    String getPlayerOneScore() {
+        return playerOneScore.getText().toString();
+    }
+    void setPlayerOneScore(String str) {
+        playerOneScore.setText(str);
+    }
+    String getPlayerTwoScore() {
+        return playerTwoScore.getText().toString();
+    }
+    void setPlayerTwoScore(String str) {
+        playerTwoScore.setText(str);
+    }
+    void incScore() {
+        if (winner == 1) {
+            setPlayerOneScore(String.valueOf(Integer.parseInt(getPlayerOneScore()) + 1));
+        } else {
+            setPlayerTwoScore(String.valueOf(Integer.parseInt(getPlayerTwoScore()) + 1));
         }
     }
-    boolean checkWinner() {
-        boolean res = false;
+    boolean validMove(Cell cell) {
+        return cell.isEmpty() && cell.getIdx() >= 0 && cell.getIdx() < cellArray.length;
+    }
+    Cell[] getEmptyCells() {
+        return cellList
+                .stream()
+                .filter(cell -> cell.isEmpty())
+                .toArray(Cell[]::new);
+    };
+    boolean hasWinner() {
+        return winner == 1 || winner == 2;
+    }
+    final void togglePlayer() {
+        playerNo = playerNo % 2 + 1;
+        playerImg = playerImg == Cell.CHAR_CROSS ? Cell.CHAR_CIRCLE : Cell.CHAR_CROSS;
+    }
+    int hasWon(int[] threeCells) {
+        int played = cellArray[threeCells[1]].getPlayed();
+        return cellArray[threeCells[0]].getPlayed() == played &&
+            played == cellArray[threeCells[2]].getPlayed() ? played : 0;
+    };
+    int getWinner() {
+        Cell[] emptyCells = getEmptyCells();
 
-        for(int[] winingPosition: winningPositions) {
-            if (!btnArray[winingPosition[0]].isEmpty() &&
-                btnArray[winingPosition[0]].getVal() == btnArray[winingPosition[1]].getVal() &&
-                btnArray[winingPosition[1]].getVal() == btnArray[winingPosition[2]].getVal()) {
-                res = true;
-            }
+        if (emptyCells.length > 4) return 0; // Game cannot be won until the 5th move
+
+        int combosLeft = WINNING_COMBOS.length;
+
+        do {
+            winner = hasWon(WINNING_COMBOS[combosLeft - 1]);
+            combosLeft--;
+        } while ((winner == 0) && (combosLeft > 0));
+        // -1 for draw, 0 for no win, 1 or 2 for winning player
+        return (winner == 0) && emptyCells.length == 0 ? -1 : winner;
+    };
+    void makeMove(Cell cell) {
+        if (validMove(cell) && !hasWinner()) {
+            cell.getComponent().setImageResource(playerImg == Cell.CHAR_CROSS ? R.drawable.cross : R.drawable.circle);
+            cell.setVal(playerNo);
+            winner = getWinner();
+            if (!hasWinner())
+                togglePlayer();
+            else
+                incScore();
         }
-        return res;
     }
     void resetGame() {
-        playerNo = 0;
-        playerImg = Btn.CHAR_CROSS;
-        thereIsAWinner = false;
-        for(int i = 0; i < btnArray.length; i++) {
-            btnArray[i].setVal(Btn.CHAR_SPACE);
-            if (btnArray[i].getComponent() != null)
-                btnArray[i].getComponent().setImageDrawable(null);
-            btnArray[i].bindComponent(null);
+        playerNo = 1;
+        playerImg = Cell.CHAR_CROSS;
+        winner = 0;
+        for(Cell cell : cellList) {
+            cell.setVal(winner);
+            if (cell.getComponent() != null)
+                cell.getComponent().setImageDrawable(null);
         }
     }
 }
